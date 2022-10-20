b9abda72c1f2cdbd40d27dd1bb9915b17b277db9
Vladimir Vejin <vejin.vladimir@gmail.com>
2022-10-20 08:27:29 +0200

    Add HelloWorld program and variables in TypeScript

---
+++ TYPESCRIPT-TRAINING/02-variables/sample-types.js
@@ -0,0 +1,13 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Vladimir";
+var lastName = 'Vejin';
+// let's break it!
+// found = 0;
+// grade = "A";
+// firstName = false;
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+//use templete Strings
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ TYPESCRIPT-TRAINING/02-variables/sample-types.ts
@@ -0,0 +1,19 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Vladimir";
+let lastName: string = 'Vejin';
+
+// let's break it!
+
+// found = 0;
+// grade = "A";
+// firstName = false;
+
+console.log(found);
+console.log("The grade is " + grade);
+
+console.log("Hi " + firstName + " "+ lastName);
+
+//use templete Strings
+console.log(`Hi ${firstName} ${lastName}`);
+
