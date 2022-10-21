bf26a8c0ae61beb11be74778de28eee857692b32
Vladimir Vejin <vejin.vladimir@gmail.com>
2022-10-21 20:23:59 +0200

    Add program for modules

---
+++ TYPESCRIPT-TRAINING/04-classes/05-modules/Driver.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Customer_1 = require("./Customer");
+// let's create an instance
+var myCustomer2 = new Customer_1.Customer("Vladimir", "Vejin");
+console.log(myCustomer2.firstName);
+console.log(myCustomer2.lastName);
---
+++ TYPESCRIPT-TRAINING/04-classes/05-modules/Driver.ts
@@ -0,0 +1,7 @@
+import {Customer} from "./Customer";
+
+// let's create an instance
+let myCustomer2 = new Customer("Vladimir","Vejin");
+
+console.log(myCustomer2.firstName);
+console.log(myCustomer2.lastName);
