4e441b9b4efe059a004335546ddef491eed9dd6e
Vladimir Vejin <vejin.vladimir@gmail.com>
2022-10-22 14:03:51 +0200

    Add code for table and add data for table

--- ANGULAR-TRAINING/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ ANGULAR-TRAINING/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  // crate an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Jovan","Maric","jovan.maric@gmail.com",9500),
+    new SalesPerson("Petar","Miric","petar.miric@gmail.com",7000),
+    new SalesPerson("Aleksandar","Petkovic","aleksandar.petkovic@gmail.com",10000),
+    new SalesPerson("Dragan","Jovic","dragan.jovic@gmail.com",8000)
+  ];
 
   constructor() { }
 
